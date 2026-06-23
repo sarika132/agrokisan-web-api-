81f93230ac6e58513080155d6a46d78b44fb3ce1
Sarika Pandey <sarika@Sarikas-MacBook-Pro.local>
2026-06-23 22:35:40 +0545

    update admin user routes

--- backend/src/routes/admin/user.route.ts
+++ backend/src/routes/admin/user.route.ts
@@ -1,18 +1,18 @@
-// import { Router } from "express";
-// import { AdminUserController } from "../../controllers/admin/user.controller";
-// // import {
-// //     adminMiddleware,
-// //     authorizedMiddleware,
-// // } from "../../middlewares/authorized.middlewares";
+import { Router } from "express";
+import { AdminUserController } from "../../controllers/admin/user.controller";
+// import {
+//     adminMiddleware,
+//     authorizedMiddleware,
+// } from "../../middlewares/authorized.middlewares";
 
-// const adminUserRoute = Router();
-// const adminUserController = new AdminUserController();
+const adminUserRoute = Router();
+const adminUserController = new AdminUserController();
 
-// // adminUserRoute.post(
-// //     "/create",
-// //     authorizedMiddleware,
-// //     adminMiddleware,
-// //     adminUserController.createUser,
-// // );
+// adminUserRoute.post(
+//     "/create",
+//     authorizedMiddleware,
+//     adminMiddleware,
+//     adminUserController.createUser,
+// );
 
-// export default adminUserRoute;
+export default adminUserRoute;
