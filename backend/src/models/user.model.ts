034aea930bc49cced537d86c54dfd5841ce3566a
Sarika Pandey <sarika@Sarikas-MacBook-Pro.local>
2026-06-23 22:26:22 +0545

    add profileImage field to user model

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -14,6 +14,7 @@ const UserMongoSchema: Schema = new Schema<IUser>(
         contactNumber: { type: String, required: true },
         password: { type: String, required: true },
         role: { type: String, enum: ["admin", "user"], default: "user" },
+        profileImage: { type: String, required: false },
     },
     {
         timestamps: true,
